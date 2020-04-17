b59a29619d2f8ec58fd24aead996c2f31b4873cd
Che Quang Huy <chehuy97@gmail.com>
2020-04-17 15:31:40 +0700

    second commit

---
+++ MovieStore/ViewControllers/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  MovieStore
+//
+//  Created by Arthur on 4/17/20.
+//  Copyright © 2020 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController()?.rearViewRevealWidth = 350
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
